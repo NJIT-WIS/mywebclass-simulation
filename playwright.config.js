45ac589fc239da57a7a82a74c438cff5c0f02686
kaw393939 <kwilliam@njit.edu>
2023-02-28 04:15:21 -0500

    fixing site

--- playwright.config.js
+++ playwright.config.js
@@ -47,16 +47,6 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] }
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
     }
 
     /* Test against mobile viewports. */

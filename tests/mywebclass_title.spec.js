d38fa2867c0e2cbdd857079597dbe4e6bb49242a
kaw393939 <kwilliam@njit.edu>
2023-02-28 04:20:32 -0500

    fixing site

--- tests/mywebclass_title.spec.js
+++ tests/mywebclass_title.spec.js
@@ -4,5 +4,5 @@ const { test, expect } = require('@playwright/test')
 test('Should have MyWebClass.org title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await page.goto('http://localhost:3000/')
-  await expect(page).toHaveTitle('MyWebClass.org a')
+  await expect(page).toHaveTitle('MyWebClass.org')
 })

6bc2daea509fb2c23a82b93d1fb6249a44f2e28e
kaw393939 <kwilliam@njit.edu>
2023-02-28 03:29:12 -0500

    optimizing site

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,91 +1,40 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const fs = require('fs')
-const TerserPlugin = require('terser-webpack-plugin')
-const ImageminPlugin = require('imagemin-webpack-plugin').default
-const imageminMozjpeg = require('imagemin-mozjpeg')
-const imageminPngquant = require('imagemin-pngquant')
-const glob = require('glob')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { PurgeCSSPlugin } = require('purgecss-webpack-plugin')
-const PATHS = {
-  src: path.join(__dirname, 'src')
-}
+
 // Look for .html files
 const htmlFiles = []
 const directories = ['src']
 while (directories.length > 0) {
   const directory = directories.pop()
-  const dirContents = fs.readdirSync(directory).map((file) =>
-    path.join(directory, file)
-  )
+  const dirContents = fs.readdirSync(directory)
+    .map(file => path.join(directory, file))
 
-  htmlFiles.push(
-    ...dirContents.filter((file) => file.endsWith('.html'))
-  )
-  directories.push(
-    ...dirContents.filter((file) => fs.statSync(file).isDirectory())
-  )
+  htmlFiles.push(...dirContents.filter(file => file.endsWith('.html')))
+  directories.push(...dirContents.filter(file => fs.statSync(file).isDirectory()))
 }
 
 module.exports = {
   mode: 'production',
-  entry: {
-    main: './src/js/main.js'
-  },
+  entry: './src/js/main.js',
   output: {
-    filename: '[name].[contenthash].js',
+    filename: 'index.js',
     path: path.resolve(__dirname, './docs'),
     clean: true
   },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    },
-    runtimeChunk: 'single',
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-        terserOptions: {
-          compress: { drop_console: true }
-        }
-      })
-    ]
+  devtool: 'source-map',
+  devServer: {
+    hot: false,
+    port: 3000
   },
   plugins: [
-    ...htmlFiles.map(
-      (htmlFile) =>
-        new HtmlWebpackPlugin({
-          template: htmlFile,
-          filename: htmlFile.replace(path.normalize('src/'), ''),
-          inject: true
-        })
-    ),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
-    new PurgeCSSPlugin({
-      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true })
-    }),
-    new ImageminPlugin({
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      plugins: [
-        imageminMozjpeg({
-          quality: 80
-        }),
-        imageminPngquant({
-          quality: [0.8, 0.9]
-        })
-      ]
-    })
+    ...htmlFiles.map(htmlFile =>
+      new HtmlWebpackPlugin({
+        template: htmlFile,
+        filename: htmlFile.replace(path.normalize('src/'), ''),
+        inject: true
+      })
+    )
   ],
   module: {
     rules: [
@@ -94,47 +43,23 @@ module.exports = {
         use: 'html-loader'
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.(js|jsx)$/,
-        include: [path.resolve(__dirname, 'src', 'client')],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              plugins: [
-                [
-                  'import',
-                  { libraryName: 'antd', style: true },
-                  'antd'
-                ]
-              ]
-            }
-          }
-        ]
-      },
-      {
         test: /\.(png|jpg|ico)$/i,
         type: 'asset',
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              pngquant: {
-                quality: [0.9, 0.95]
-              }
+        use: [{
+          loader: 'image-webpack-loader',
+          options: {
+            pngquant: {
+              quality: [0.90, 0.95]
             }
           }
-        ],
+        }],
         parser: {
           dataUrlCondition: {
             maxSize: 10 * 1024 // 10kb
           }
         },
         generator: {
-          filename: 'assets/images/[name]-[contenthash][ext]'
+          filename: 'assets/images/[name]-[hash][ext]'
         }
       },
       {

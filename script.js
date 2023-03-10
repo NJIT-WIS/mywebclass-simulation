6d9304ae45ab990a6201856ff2f47943fb2917eb
kaw393939 <kwilliam@njit.edu>
2023-03-10 14:46:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  const report = fs.readFileSync('test-results/lighthousereport.json', 'utf8');
+  const reportObj = JSON.parse(report);
+
+  await page.setContent(reportObj.report);
+  await page.pdf({ path: 'test-results/lighthouse/report.pdf', format: 'A4' });
+
+  await browser.close();
+})();

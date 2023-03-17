847c275598021f8faf1ab16f1504a9e4db6df93d
Kevin Nipal <kmn6@njit.edu>
2023-03-17 19:55:21 -0400

    remove expect, not needed. Add detailed report options

--- tests/accessibility.spec.js
+++ tests/accessibility.spec.js
@@ -1,30 +1,32 @@
-const { test, expect } = require('@playwright/test')
+const { test } = require('@playwright/test')
 const { axe, injectAxe, checkA11y } = require('axe-playwright')
 
 test('test-accessibility-homepage', async ({ page }) => {
-  // Navigate to the home page
-  await page.goto('/');
+    // Navigate to the home page
+    await page.goto('/');
 
-  // Inject axe into the page
-  await injectAxe(page);
+    // Inject axe into the page
+    await injectAxe(page);
 
-  // Run axe on the page
-  const results = await checkA11y(page);
-
-  // Check for accessibility violations
-  expect(results.violations.length).toBe(0);
+    // Run axe on the page
+    const results = await checkA11y(page, null, {
+        axeOptions: {},
+        detailedReport: true,
+        detailedReportOptions: {html: true}
+    });
 });
 
 test('test-accessibility-template-content', async ({ page }) => {
-  // Navigate to the home page
-  await page.goto('/templates/template-content.html');
+    // Navigate to the home page
+    await page.goto('/templates/template-content.html');
 
-  // Inject axe into the page
-  await injectAxe(page);
+    // Inject axe into the page
+    await injectAxe(page);
 
-  // Run axe on the page
-  const results = await checkA11y(page);
-
-  // Check for accessibility violations
-  expect(results.violations.length).toBe(0);
+    // Run axe on the page
+    const results = await checkA11y(page, null, {
+        axeOptions: {},
+        detailedReport: true,
+        detailedReportOptions: {html: true}
+    });
 });

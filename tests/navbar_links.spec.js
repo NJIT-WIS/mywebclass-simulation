ab42d871bf0f4a2d1d11fac8a158c5c02a44bdc6
Kevin Nipal <kmn6@njit.edu>
2023-03-17 15:27:13 -0400

    add the test that checks the wording and links of the navbar

--- tests/navbar_links.spec.js
+++ tests/navbar_links.spec.js
@@ -1,29 +1,18 @@
 const { test, expect } = require('@playwright/test')
 
+const pagesToCheck = ['/',
+                      '/templates/template-content.html',
+]
 
 test('test-valid-navbar-links', async ({browser}) => {
-
-//    const pagesToCheck = ['http://localhost:3000',
-//                          'http://localhost:3000/templates/template-content.html',
-//    ]
     const context = await browser.newContext()
-    const page = await context.newPage();
-
-    const pagesToCheck = ['/',
-                          '/templates/template-content.html',
-    ]
+    const page = await context.newPage()
 
     for (const pageToCheck of pagesToCheck) {
         console.log("Checking page " + pageToCheck)
 
         await page.goto(pageToCheck)
-
-
         const links = await page.$$(".navbar .nav-link")
-
-        // Click on a button with id 'agreeButton'
-        //do I need this?
-        //await page.click('#agreeButton')
 
         for (const link of links) {
             const url = await link.getAttribute('href')
@@ -33,12 +22,37 @@ test('test-valid-navbar-links', async ({browser}) => {
                 continue
             }
 
-            const newPage = await context.newPage();
+            const newPage = await context.newPage()
             const response = await newPage.goto(url)
             const status = response.status()
             expect(response.ok()).toBe(true)
 
             await newPage.close()
         }
+    }
+})
+
+test.only('navbar-contains-correct-links-and-wording', async ({page}) => {
+    const expectedNavbarLinks = [ ["Home", "/"],
+                                  ["Content Template", "/templates/template-content.html"]
+    ]
+
+    for (const pageToCheck of pagesToCheck) {
+        console.log("Checking page " + pageToCheck)
+
+        await page.goto(pageToCheck)
+        const links = await page.$$(".navbar .nav-link")
+
+        for (let i = 0; i < links.length; i++) {
+            const link = links[i]
+            const textContent = await link.textContent()
+            const linkedUrl = await link.getAttribute("href")
+
+            console.log("Checking for " + expectedNavbarLinks[i][0] + " linking to " + expectedNavbarLinks[i][1])
+            expect (textContent).toBe(expectedNavbarLinks[i][0])
+            expect (linkedUrl).toBe(expectedNavbarLinks[i][1])
+
+        }
+
     }
 })

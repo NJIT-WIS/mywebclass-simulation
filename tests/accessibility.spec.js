5825a6ec3bbad4e8556e057a9f34c07250c49d85
jnarlyv <jeanarly@gmail.com>
2023-03-21 22:04:12 -0400

    created a playwright test that check out accessbility

---
+++ tests/accessibility.spec.js
@@ -0,0 +1,36 @@
+const { test, expect } = require('@playwright/test')
+
+test('Check Accessibility and Usability', async ({ page }) => {
+  await page.goto('http://localhost:3000')
+
+  // Ensure page has a title
+  const pageTitle = await page.title()
+  expect(pageTitle).toBeTruthy()
+
+  // Ensure all images have alt attributes
+  const images = await page.$$('img')
+  for (const image of images) {
+    expect(await image.getAttribute('alt')).toBeTruthy()
+  }
+
+  // Ensure all form fields have labels
+  const formFields = await page.$$('input, select, textarea')
+  for (const field of formFields) {
+    const label = await page.$(`label[for="${await field.getAttribute('id')}"]`)
+    expect(label).toBeTruthy()
+  }
+
+  // Ensure all links have descriptive text
+  const links = await page.$$('a')
+  for (const link of links) {
+    const text = await link.innerText()
+    expect(text).toBeTruthy()
+  }
+
+  // Ensure there are no empty headings
+  const headings = await page.$$('h1, h2, h3, h4, h5, h6')
+  for (const heading of headings) {
+    const text = await heading.innerText()
+    expect(text).toBeTruthy()
+  }
+})

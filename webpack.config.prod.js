c4601f64a588ebbc7f9c258d287e5987d72300fd
Nathan Hernandez <nathandhernandez@gmail.com>
2023-03-22 11:13:42 -0400

    Refactor Webpack configs to allow emitting of robots.txt and sitemap.xml files. Additional refactoring as follows: * Remove extra linting and standard js from package.json. * Custom linting setup and prettier were begin applied. * Also makes a correction so tests run with new linting setup. * Fixes broken navigation links.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,17 +1,17 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const fs = require('fs')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+const prefixer = require('autoprefixer');
 
 // Look for .html files
-const htmlFiles = []
-const directories = ['src']
+const htmlFiles = [];
+const directories = ['src'];
 while (directories.length > 0) {
-  const directory = directories.pop()
-  const dirContents = fs.readdirSync(directory)
-    .map(file => path.join(directory, file))
+  const directory = directories.pop();
+  const dirContents = fs.readdirSync(directory).map((file) => path.join(directory, file));
 
-  htmlFiles.push(...dirContents.filter(file => file.endsWith('.html')))
-  directories.push(...dirContents.filter(file => fs.statSync(file).isDirectory()))
+  htmlFiles.push(...dirContents.filter((file) => file.endsWith('.html')));
+  directories.push(...dirContents.filter((file) => fs.statSync(file).isDirectory()));
 }
 
 module.exports = {
@@ -20,80 +20,95 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, './docs'),
-    clean: true
+    clean: true,
   },
   devtool: 'source-map',
   plugins: [
-    ...htmlFiles.map(htmlFile =>
-      new HtmlWebpackPlugin({
-        template: htmlFile,
-        filename: htmlFile.replace(path.normalize('src/'), ''),
-        inject: true
-      })
-    )
+    ...htmlFiles.map(
+      (htmlFile) =>
+        new HtmlWebpackPlugin({
+          template: htmlFile,
+          filename: htmlFile.replace(path.normalize('src/'), ''),
+          inject: true,
+        }),
+    ),
   ],
   module: {
     rules: [
       {
         test: /\.html$/i,
-        use: 'html-loader'
+        use: 'html-loader',
       },
       {
         test: /\.(png|jpg|ico)$/i,
         type: 'asset',
-        use: [{
-          loader: 'image-webpack-loader',
-          options: {
-            pngquant: {
-              quality: [0.90, 0.95]
-            }
-          }
-        }],
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              pngquant: {
+                quality: [0.9, 0.95],
+              },
+            },
+          },
+        ],
         parser: {
           dataUrlCondition: {
-            maxSize: 10 * 1024 // 10kb
-          }
+            maxSize: 10 * 1024, // 10kb
+          },
         },
         generator: {
-          filename: 'assets/images/[name]-[hash][ext]'
-        }
+          filename: 'assets/images/[name]-[hash][ext]',
+        },
       },
       {
         test: /\.(webmanifest)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/i,
         type: 'asset/resource',
         generator: {
           // filename: 'fonts/[name]-[hash][ext][query]'
-          filename: 'fonts/[name][ext][query]'
-        }
+          filename: 'fonts/[name][ext][query]',
+        },
       },
       {
         test: /\.(scss)$/,
-        use: [{
-          loader: 'style-loader'
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [prefixer],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(xml)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'sitemap.xml',
         },
-        {
-          loader: 'css-loader'
+      },
+      {
+        test: /\.(txt)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'robots.txt',
         },
-        {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              plugins: () => [
-                require('autoprefixer')
-              ]
-            }
-          }
-        },
-        {
-          loader: 'sass-loader'
-        }
-
-        ]
-      }]
-  }
-
-}
+      },
+    ],
+  },
+};

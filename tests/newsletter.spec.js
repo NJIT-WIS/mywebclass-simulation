fe583489dd9bf04d7d865459382d1f25968c66df
jnarlyv <jeanarly@gmail.com>
2023-03-21 23:40:35 -0400

    created subscribe and newsletter test

--- tests/newsletter.spec.js
+++ tests/newsletter.spec.js
@@ -1,11 +1,11 @@
 const { test, expect } = require('@playwright/test')
 
-test('newsletter subscription', async ({ page }) => {
+test('submit newsletter form', async ({ page }) => {
   await page.goto('http://localhost:3000')
-  await page.waitForSelector('#newsletter-form')
-  await page.fill('#newsletter-form input[name="email"]', 'test@example.com')
-  await page.click('#newsletter-form button[type="submit"]')
+  await page.waitForSelector('form[id="newsletter-form"]')
+  await page.fill('form[id="newsletter-form"] input[name="email"]', 'test@example.com')
+  await page.click('form[id="newsletter-form"] button[type="submit"]')
   const successMessage = await page.waitForSelector('#newsletter-success-message')
-  const successText = await successMessage.innerText()
-  expect(successText).toContain('Thank you for subscribing!')
+  const successMessageText = await successMessage.textContent()
+  expect(successMessageText).toContain('Thanks for subscribing!')
 })

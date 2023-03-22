6920dab941a4982c45d9e3509d3912822911073c
jnarlyv <jeanarly@gmail.com>
2023-03-21 23:36:00 -0400

    created subscribe and newsletter test

--- tests/newsletter.spec.js
+++ tests/newsletter.spec.js
@@ -1,21 +1,11 @@
 const { test, expect } = require('@playwright/test')
 
 test('newsletter subscription', async ({ page }) => {
-  // Navigate to the website
   await page.goto('http://localhost:3000')
-
-  // Click the newsletter sign-up button
-  const newsletterButton = await page.waitForSelector('#newsletter-btn')
-  await newsletterButton.click()
-
-  // Fill out the sign-up form
-  await page.waitForSelector('#email-input')
-  await page.type('#email-input', 'test@example.com')
-  const submitButton = await page.waitForSelector('#submit-btn')
-  await submitButton.click()
-
-  // Verify that the confirmation message is displayed
-  const confirmationMessage = await page.waitForSelector('#confirmation-message')
-  const messageText = await confirmationMessage.innerText()
-  expect(messageText).toContain('Thank you for subscribing!')
+  await page.waitForSelector('#newsletter-form')
+  await page.fill('#newsletter-form input[name="email"]', 'test@example.com')
+  await page.click('#newsletter-form button[type="submit"]')
+  const successMessage = await page.waitForSelector('#newsletter-success-message')
+  const successText = await successMessage.innerText()
+  expect(successText).toContain('Thank you for subscribing!')
 })

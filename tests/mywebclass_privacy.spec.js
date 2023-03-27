c6f37a7a8ad7f554f8a9621bc1f932402650600e
Suman Mhalsank <48148023+Sumanmhalsank02@users.noreply.github.com>
2023-03-27 12:06:45 -0400

    TEST : For privacy policy

--- tests/mywebclass_privacy.spec.js
+++ tests/mywebclass_privacy.spec.js
@@ -2,11 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('Privacy policy exists', async ({ page }) => {
   // Go to the localhost:3000
-  await page.goto('http://localhost:3000/privacy.html');
+  await page.goto('http://localhost:3000);
 
   // Find the link to the privacy policy
   const privacyPolicyLink = await page.$('a[href="/privacy.html"]');
 
   // Check if the link exists
-  expect(privacyPolicyLink).not.toBeNull();
+  expect(privacyPolicyLink).not.toBeDefined();
 });
+

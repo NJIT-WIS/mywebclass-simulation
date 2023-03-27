74983416ca561cb63b32603d23ab6970af9b7f2e
lt235 <lt235@nijt.edu>
2023-03-27 13:32:37 -0400

    Added test for privacy policy

--- tests/mywebclass_privacypolicy.spec.js
+++ tests/mywebclass_privacypolicy.spec.js
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://mywebclass.org/');
-  await page.getByRole('button', { name: 'Agree', exact: true }).click();
-  await expect(page).toHaveText('Privacy Policy');
+  await page.getByRole('button', { name: 'Agree', exact: true }).click()
+  await expect(page).toHaveText('Privacy Policy')
 });

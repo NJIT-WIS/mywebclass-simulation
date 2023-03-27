20e4cf5efe5cc991007329024c2789aea739d223
mp299 <114026858+mp299@users.noreply.github.com>
2023-03-27 12:05:15 -0400

    first test for title

---
+++ tests/test1.spec.js
@@ -0,0 +1,28 @@
+// @ts-check
+const { chromium } = require('playwright');
+
+describe('Open website', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await chromium.launch();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  it('should open website', async () => {
+    await page.goto('https://mp299.github.io/mywebclass-simulation/');
+    expect(await page.title()).toContain('MyWebClass Homepage');
+  });
+});

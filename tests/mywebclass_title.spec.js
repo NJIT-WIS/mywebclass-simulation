2a6eb36f6ba95f24a36301f64a833272a2f8d2e8
ZachGG421 <chubbizg@gmail.com>
2023-03-27 19:05:11 -0400

    added test 3

--- tests/mywebclass_title.spec.js
+++ tests/mywebclass_title.spec.js
@@ -18,3 +18,11 @@ test('Test Case 2: Accepting Privacy Policy', async ({ page }) => {
 
     expect(page.url()).toContain('https://kaanismet.github.io/mywebclass-simulation/');
 });
+
+test('Test Case 3: Click on Templates', async ({ page }) => {
+    await page.goto('https://kaanismet.github.io/mywebclass-simulation/');
+    await page.getByRole('link', { name: 'Content Template' }).click();
+
+    expect(page.url()).toBe('https://kaanismet.github.io/mywebclass-simulation/templates.html');
+
+});

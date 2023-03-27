17c7c8c25e33d236f8014774916de8545cd948b4
ZachGG421 <chubbizg@gmail.com>
2023-03-27 15:03:41 -0400

    updated test 1

--- tests/test.js
+++ tests/test.js
@@ -13,18 +13,19 @@ describe('Example Test Suite', () => {
         await browser.close();
     });
 
-    test('Test Case 1: Open Privacy Policy', async () => {
-    await page.goto('https://kaanismet.github.io/mywebclass-simulation/');
-    const page1Promise = page.waitForEvent('popup');
-    await page.locator('#privacyModal').getByRole('link', { name: 'Privacy Policy' }).click();
-    const page1 = await page1Promise;
+    describe('Home page', () => {
+        test('Test Case 1: Open Privacy Policy', async () => {
+            await page.goto('https://kaanismet.github.io/mywebclass-simulation/');
+            const page1Promise = page.waitForEvent('popup');
+            await page.locator('#privacyModal').getByRole('link', { name: 'Privacy Policy' }).click();
+            const page1 = await page1Promise;
 
+            expect(page1.url()).toBe('https://kaanismet.github.io/mywebclass-simulation/privacy.html');
+        });
 
-    expect(page1.url()).toBe('https://kaanismet.github.io/mywebclass-simulation/privacy.html');
+        test('Test Case 2: Accepting Privacy Policy', async () => {
+            await page.goto('https://kaanismet.github.io/mywebclass-simulation/');
+            await page.getByRole('button', { name: 'Agree', exact: true }).click();
+        });
     });
-
-    test('Test Case 2: Accepting Privacy Policy', async () => {
-    await page.goto('https://kaanismet.github.io/mywebclass-simulation/');
-    await page.getByRole('button', { name: 'Agree', exact: true }).click();
-    })
-  });
+});

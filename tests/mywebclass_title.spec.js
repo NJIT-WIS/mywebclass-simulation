cbd22e86c55df7030b5bf200e83b6d385055bea3
ZachGG421 <chubbizg@gmail.com>
2023-03-27 18:45:26 -0400

    updating test

--- tests/mywebclass_title.spec.js
+++ tests/mywebclass_title.spec.js
@@ -9,14 +9,12 @@ test('Test Case 1: check privacy policy', async ({ page }) => {
     const page1 = await page1Promise;
 
     expect(page1.url()).toBe('https://kaanismet.github.io/mywebclass-simulation/privacy.html');
-    //await page.goto('http://localhost:3000')
-    //await expect(page).toHaveTitle('MyWebClass.org')
 })
 
 test('Test Case 2: Accepting Privacy Policy', async ({ page }) => {
     await page.goto('https://kaanismet.github.io/mywebclass-simulation/');
     await page.getByRole('button', { name: 'Agree', exact: true }).click();
 
-    const page1 = await page1Promise;
-    expect(page1.url()).toContain('https://kaanismet.github.io/mywebclass-simulation/');
+
+    expect(page.url()).toContain('https://kaanismet.github.io/mywebclass-simulation/');
 });

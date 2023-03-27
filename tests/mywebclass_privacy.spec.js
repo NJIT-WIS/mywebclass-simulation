f0d7a8fcca297dc3aa366d4f0d0c3dbbf0a3fb3e
Suman Mhalsank <48148023+Sumanmhalsank02@users.noreply.github.com>
2023-03-27 13:10:36 -0400

    TEST: privacy playwright

--- tests/mywebclass_privacy.spec.js
+++ tests/mywebclass_privacy.spec.js
@@ -1,13 +1,10 @@
-import { test, expect } from '@playwright/test';
+// @ts-check
+const { test, expect } = require('@playwright/test')
 
-test('Privacy policy exists', async ({ page }) => {
-  // Go to the localhost:3000
-  await page.goto('http://localhost:3000);
-
-  // Find the link to the privacy policy
-  const privacyPolicyLink = await page.$('a[href="/privacy.html"]');
-
-  // Check if the link exists
-  expect(privacyPolicyLink).not.toBeDefined();
-});
+test('Privacy policy should be present on website', async ({ page }) => {
+  await page.goto('http://localhost:3000')
+  // Check if the privacy policy link is present
+  const privacyPolicyLink = await page.$('a[href="privacy.html"]')
+  expect(privacyPolicyLink).toBeDefined()
+})
 

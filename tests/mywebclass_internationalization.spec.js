1ba41735a498e48ec02eb0063736de728580cf54
Suman Mhalsank <48148023+Sumanmhalsank02@users.noreply.github.com>
2023-03-27 11:01:11 -0400

    FIXED: playwright test

--- tests/mywebclass_internationalization.spec.js
+++ tests/mywebclass_internationalization.spec.js
@@ -12,14 +12,14 @@ test.describe('MyWebClass.org internationalization', () => {
     await page.close()
   })
 
-  test('should display the site in "en" when the "Accept-Language" header is set to "en"', async () => {
+  test('should display the site in "en" when the "Accept-Language" header is set to "EN"', async () => {
     // Set the Accept-Language header and navigate to the website
-    await page.setExtraHTTPHeaders({ 'Accept-Language': 'en' })
+    await page.setExtraHTTPHeaders({ 'Accept-Language': 'EN' })
     await page.goto('http://localhost:3000')
 
     // Check if the lang attribute of the HTML tag is set to "en"
     const langAttribute = await page.getAttribute('html', 'lang')
-    expect(langAttribute).toBe('en', 'The lang attribute of the HTML tag is not set to "en".')
+    expect(langAttribute).toBe('EN', 'The lang attribute of the HTML tag is not set to "EN".')
 
     // Perform any additional checks for the "en" language version of the website here, e.g., checking specific text or elements
   })

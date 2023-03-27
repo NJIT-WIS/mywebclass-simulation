41860cf5125f185b960153d33cdd31f5a65f13ea
ZachGG421 <chubbizg@gmail.com>
2023-03-26 20:52:44 -0400

    completed test 1

--- tests/test.js
+++ tests/test.js
@@ -14,10 +14,12 @@ describe('Example Test Suite', () => {
     });
 
     test('Test Case 1', async () => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('https://kaanismet.github.io/mywebclass-simulation/');
     const page1Promise = page.waitForEvent('popup');
     await page.locator('#privacyModal').getByRole('link', { name: 'Privacy Policy' }).click();
     const page1 = await page1Promise;
 
+
+    expect(page1.url()).toBe('https://kaanismet.github.io/mywebclass-simulation/privacy.html');
     });
   });

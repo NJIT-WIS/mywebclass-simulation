b5e10413037bd6daef9c549505c0d803dd0fc8e4
lt235 <lt235@nijt.edu>
2023-03-27 15:47:35 -0400

    Retry #3

--- tests/mywebclass_privacypolicy.spec.js
+++ tests/mywebclass_privacypolicy.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://mywebclass.org/');
+test('Should have header of Privacy Policy', async ({ page }) => {
+  await page.goto('http://localhost:3000');
   await page.getByRole('button', { name: 'Agree', exact: true }).click();
   await page.getByRole('link', { name: 'Privacy Policy' }).click();
   const headerText = await page.$eval('header', el => el.innerText);

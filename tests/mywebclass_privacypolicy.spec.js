3efe143608c76fe748cb7600640885ac3d0d1554
lt235 <lt235@nijt.edu>
2023-03-27 13:48:42 -0400

    Updated test for privacy policy, should work now

--- tests/mywebclass_privacypolicy.spec.js
+++ tests/mywebclass_privacypolicy.spec.js
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://mywebclass.org/');
-  await page.getByRole('button', { name: 'Agree', exact: true }).click()
-  await expect(page).toHaveText('Privacy Policy')
+  await page.getByRole('button', { name: 'Agree', exact: true }).click();
+  await page.getByRole('link', { name: 'Privacy Policy' }).click();
+  const headerText = await page.$eval('header', el => el.innerText);
+  expect(headerText).toContain('Privacy Policy');
 });

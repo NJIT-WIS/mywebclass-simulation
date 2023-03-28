0c7894502667b09a91eadb6671b2b62a56d3ff07
lt235 <lt235@nijt.edu>
2023-03-27 21:31:06 -0400

    Removed test to detect privacy policy page, created test to check for content template

---
+++ tests/mywebclass_contenttemplate.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@playwright/test')
+
+test('Should have header of Article Title', async ({ page }) => {
+  await page.goto('http://localhost:3000')
+  await page.getByRole('button', { name: 'Agree', exact: true }).click()
+  await page.getByRole('link', { name: 'Content Template' }).click()
+  await page.waitForSelector('header')
+  // Check if header tag contains "Privacy Policy"
+  const headerText = await page.$eval('h1', el => el.innerText)
+  await expect(headerText).toContain('Article Title')
+})
--- tests/mywebclass_privacypolicy.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Should have header of Privacy Policy', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  await page.getByRole('button', { name: 'Agree', exact: true }).click();
-  await page.getByRole('link', { name: 'Privacy Policy' }).click();
-  const headerText = await page.$eval('header', el => el.innerText);
-  await expect(headerText).toContain('Privacy Policy');
-});

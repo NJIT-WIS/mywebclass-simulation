dd6a4cd53e9deb476fc96a865cd496ed0ebce51d
ZachGG421 <chubbizg@gmail.com>
2023-03-27 21:36:44 -0400

    update test 5 and enter test 6

--- tests/mywebclass_title.spec.js
+++ tests/mywebclass_title.spec.js
@@ -34,6 +34,7 @@ test('Test Case 4: Click on Our Story', async ({ page }) => {
     expect(page.url()).toBe('https://kaanismet.github.io/mywebclass-simulation/story.html');
 });
 
+/*
 test('Test Case 5: Click on Privacy Policy', async ({ page }) => {
     await page.goto('https://kaanismet.github.io/mywebclass-simulation/');
     await page.getByRole('button', { name: 'Agree', exact: true }).click();
